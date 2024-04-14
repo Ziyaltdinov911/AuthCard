050818c95af0391c5fdcb64f88591f3d8e10f70c
NSaveMe <_email@example.com>
2024-04-14 23:07:26 +0500

    Initial Commit

---
+++ AppWithFB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppWithFB
+//
+//  Created by Камиль Байдиев on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
